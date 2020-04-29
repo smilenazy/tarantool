0c0caa9fba0ce58b3e6cf510a501bb97ffbaeb58
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2020-04-29 23:33:17 +0200

    replication: make sync transactions wait quorum
    Synchronous transaction (which changes anything in a synchronous
    space) before commit waits until it is replicated onto a quorum
    of replicas.
    
    So far all the 'synchronousness' is basically the same as the well
    known 'wait_lsn' technique. With the exception, that the
    transaction really is not committed until replicated.
    
    Problem of wait_lsn is still present though, in case master
    restarts. Because there is no a 'confirm' record in WAL telling
    which transactions are replicated and can be applied.
    
    Closes #4844
    Closes #4845

--- src/box/box.cc
+++ src/box/box.cc
@@ -59,6 +59,7 @@
 #include "index.h"
 #include "port.h"
 #include "txn.h"
+#include "txn_limbo.h"
 #include "user.h"
 #include "cfg.h"
 #include "coio.h"
@@ -2388,6 +2389,7 @@ box_init(void)
 	if (tuple_init(lua_hash) != 0)
 		diag_raise();
 
+	txn_limbo_init();
 	sequence_init();
 }
 

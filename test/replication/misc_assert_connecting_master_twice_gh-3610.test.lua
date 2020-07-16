1555effabee99a1abca07439168c5c683f97638e
Alexander V. Tikhonov <avtikhon@tarantool.org>
2020-07-16 14:42:40 +0300

    Divide replication/misc.test.lua
    To fix flaky issues of replication/misc.test.lua the test had to be
    divided into smaller tests to be able to localize the flaky results:
    
      misc_assert_connecting_master_twice_gh-3610.test.lua
      misc_assert_on_server_die_gh-2991.test.lua
      misc_assert_replica_on_applier_disconnect_gh-3510.test.lua
      misc_crash_on_box_concurrent_update_gh-3606.test.lua
      misc_heartbeats_on_master_changes_gh-3160.test.lua
      misc_no_failure_on_error_reading_wal_gh-4399.test.lua
      misc_no_panic_on_connected_gh-3637.test.lua
      misc_no_restart_on_same_configuration_gh-3711.test.lua
      misc_no_socket_leak_on_replica_disconnect_gh-3642.test.lua
      misc_orphan_on_reconfiguration_error_gh-4424.test.lua
      misc_rebootstrap_from_ro_master_gh-3111.test.lua
      misc_replica_checks_cluster_id_gh-3704.test.lua
      misc_return_on_quorum_0_gh-3760.test.lua
      misc_value_not_replicated_on_iproto_request_gh-3247.test.lua
    
    Needed for #4940

---
+++ test/replication/misc_assert_connecting_master_twice_gh-3610.test.lua
@@ -0,0 +1,41 @@
+test_run = require('test_run').new()
+test_run:cmd("restart server default")
+uuid = require('uuid')
+fiber = require('fiber')
+
+replication_connect_quorum = box.cfg.replication_connect_quorum
+replication_connect_timeout = box.cfg.replication_connect_timeout
+
+--
+-- Test case for gh-3610. Before the fix replica would fail with the assertion
+-- when trying to connect to the same master twice.
+--
+box.schema.user.grant('guest', 'replication')
+test_run:cmd("create server replica with rpl_master=default, script='replication/replica.lua'")
+test_run:cmd("start server replica")
+test_run:cmd("switch replica")
+replication = box.cfg.replication[1]
+box.cfg{replication = {replication, replication}}
+
+-- Check the case when duplicate connection is detected in the background.
+test_run:cmd("switch default")
+listen = box.cfg.listen
+box.cfg{listen = ''}
+
+test_run:cmd("switch replica")
+box.cfg{replication_connect_quorum = 0, replication_connect_timeout = 0.01}
+box.cfg{replication = {replication, replication}}
+
+test_run:cmd("switch default")
+box.cfg{listen = listen}
+while test_run:grep_log('replica', 'duplicate connection') == nil do fiber.sleep(0.01) end
+
+test_run:cmd("stop server replica")
+test_run:cmd("cleanup server replica")
+test_run:cmd("delete server replica")
+test_run:cleanup_cluster()
+box.schema.user.revoke('guest', 'replication')
+
+box.cfg{replication = "", \
+        replication_connect_quorum = replication_connect_quorum, \
+        replication_connect_timeout = replication_connect_timeout}
